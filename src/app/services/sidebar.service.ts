5f1c356424b4808d7fe3cab330f436e199268e25
Daniel Romero AG <48894430+DanielTuttos@users.noreply.github.com>
2020-04-23 23:38:02 -0500

    Fin seccion 17

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,33 +1,43 @@
 import { Injectable } from '@angular/core';
+import { UsuarioService } from './usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SidebarService {
 
-  menu: any = [
-    {
-      titulo: 'Principal',
-      icono: 'mdi mdi-gauge', 
-      submenu: [
-        { titulo: 'Dashboard', url: '/dashboard' },
-        { titulo: 'ProgressBar', url: '/progress' },
-        { titulo: 'Graficas', url: '/graficas1' },
-        { titulo: 'Promesas', url: '/promesas' },
-        { titulo: 'RXJS', url: '/rxjs' }
-      ]
-    },
-    {
-      titulo: 'Mantenimiento',
-      icono: 'mdi mdi-folder-lock-open',
-      submenu:[
-        {titulo: 'Usuarios', url: '/usuarios'},
-        {titulo: 'Hospitales', url: '/hospitales'},
-        {titulo: 'Medicos', url: '/medicos'},
+  menu: any[] = [];
 
-      ]
-    }
-  ];
+  // menu: any = [
+  //   {
+  //     titulo: 'Principal',
+  //     icono: 'mdi mdi-gauge', 
+  //     submenu: [
+  //       { titulo: 'Dashboard', url: '/dashboard' },
+  //       { titulo: 'ProgressBar', url: '/progress' },
+  //       { titulo: 'Graficas', url: '/graficas1' },
+  //       { titulo: 'Promesas', url: '/promesas' },
+  //       { titulo: 'RXJS', url: '/rxjs' }
+  //     ]
+  //   },
+  //   {
+  //     titulo: 'Mantenimiento',
+  //     icono: 'mdi mdi-folder-lock-open',
+  //     submenu:[
+  //       {titulo: 'Usuarios', url: '/usuarios'},
+  //       {titulo: 'Hospitales', url: '/hospitales'},
+  //       {titulo: 'Medicos', url: '/medicos'},
 
-  constructor() { }
+  //     ]
+  //   }
+  // ];
+
+  constructor(private _usuarioService: UsuarioService) {
+  }
+
+  cargarMenu() {
+    this.menu = this._usuarioService.menu;
+
+
+  }
 }
